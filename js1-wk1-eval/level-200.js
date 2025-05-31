7b1a91588fda3f11a7a128367de2b8c89097dbee
PawelBroilo <88758214+PawelBroilo@users.noreply.github.com>
2025-05-31 11:27:49 +0100

    Update level-200.js

--- js1-wk1-eval/level-200.js
+++ js1-wk1-eval/level-200.js
@@ -7,6 +7,19 @@
 // b) 170.105
 // c) -0.34
 // d) -8.921
+// a)Math.trunc10
+// a)Math.floor10
+// b)Math.trunc170
+// b)Math.floor170
+// c)Math.trunc 0
+// c)math.floor 0
+// d)math.trunc 8
+// d.math.floor 8
+
+
+
+
+
 
 // Part 2: Use the Node REPL to check your answers to Part 1)
 // Part 3: Using Part 2, explain the difference between Math.trunc() and Math.floor() in your own words
